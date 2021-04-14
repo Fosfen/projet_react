96bda643578fc9b99214663ff7520c8bfad3964b
raphGit <d.rapha.rd@gmail.com>
2021-04-14 15:14:51 +0200

    FEAT : Home view added

---
+++ Views/Home.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const  Home = () => {
+  return (
+    <View style={styles.main_container}>
+        <Text>Main container</Text>
+        
+        <View style={styles.title_container}>
+            <Text>Title container</Text>
+        </View>
+
+        <View style={styles.dishes_container}>
+         {/* DISHES COME HERE */}
+        </View>
+      
+      </View>
+  );
+};
+
+const styles = StyleSheet.create({
+    main_container: {
+        backgroundColor:"red"
+    },
+    title_container:{
+        padding:3,
+        backgroundColor:"green"
+    },
+    dishes_container:{
+        flexDirection: "row",
+        backgroundColor:"grey"
+    },
+  });
+  
+
+export default Home;
