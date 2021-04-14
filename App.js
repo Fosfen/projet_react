c957e57b2f51b21112d05911d82ea210c9eb3cdc
raphGit <d.rapha.rd@gmail.com>
2021-04-14 15:29:12 +0200

    Chore : React Navigation Added

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import Home from './Views/Home';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <Home/>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{ title: 'Delivecrous'}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
