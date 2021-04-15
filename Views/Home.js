0ed8e22a91249d6da1996898a0a449dfd8255303
raphGit <d.rapha.rd@gmail.com>
2021-04-15 10:23:50 +0200

    FEAT : Dishes List with request added

--- Views/Home.js
+++ Views/Home.js
@@ -1,7 +1,17 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, Text, StyleSheet, FlatList } from "react-native";
 
 const  Home = () => {
+  
+  const [data, setData] = useState([]);
+  
+  useEffect(() => {
+    fetch('http://localhost:3000/dishes')
+      .then((response) => response.json())
+      .then((json) => setData(json))
+      .catch((error) => console.error(error));
+  }, []);
+
   return (
     <View>
         <View style={styles.title_container}>
@@ -10,6 +20,15 @@ const  Home = () => {
 
         <View style={styles.dishes_container}>
          {/* DISHES COME HERE */}
+         <FlatList
+          data={data}
+          keyExtractor={({ id }, index) => id}
+          renderItem={({ item }) => (
+            <Text>{item.name}</Text>
+          )}
+        />
+
+
         </View>
       </View>
   );
