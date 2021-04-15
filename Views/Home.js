997855ebbda1c2a76ffc711f054001674a637d96
Sebastien Delhalle <sebastien.delhalle@colisweb.com>
2021-04-15 12:28:11 +0200

    Feat: Added description to dishes

--- Views/Home.js
+++ Views/Home.js
@@ -4,8 +4,8 @@ import Dish from "../components/dish";
 
 const mocked_data = {
   dishes: [
-    { "id": "1", "name": "Saucisse" },
-    { "id": "2", "name": "Kebab" }
+    { "id": "1", "name": "Saucisse", "description": "Une onctueuse saucisse, dotée d'une délicate touche de miel." },
+    { "id": "2", "name": "Kebab", "description": "C'est la street, on aime tous graille un bon Kebab" }
   ]}
 
 const  Home = () => {
@@ -26,7 +26,7 @@ const  Home = () => {
           data={data}
           keyExtractor={({ id }, index) => id}
           renderItem={({ item }) => (
-            <Dish name={item.name}/>
+            <Dish name={item.name} description={item.description}/>
           )}
         />
         </View>
